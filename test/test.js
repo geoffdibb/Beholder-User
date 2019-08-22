04e9e11398b1eacab67300516a3d5ca164b66e2a
lushilling <lushilling@hotmail.co.uk>
2019-08-22 15:16:27 +0100

    install chai

--- test/test.js
+++ test/test.js
@@ -1,8 +1,20 @@
+var auditRequestLog = require('../routes/auditRequestLog');
+var expect = require('chai').expect;
+
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+
+describe('Array', function () {
+    describe('indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal([1, 2, 3].indexOf(2), 1);
+        });
     });
-  });
 });
+
+it('should return -1 when the value is not present', function () {
+    assert.equal([1, 2, 3].indexOf(1), 0);
+});
+
+it('function should ... ', function(){
+    assert.equal();
+})
