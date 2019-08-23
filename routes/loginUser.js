90b6baf4d251061771973c90a04f12f251868f42
geoffdibb <geoffdibb@yahoo.co.uk>
2019-08-23 17:03:49 +0100

    added user login audit

--- routes/loginUser.js
+++ routes/loginUser.js
@@ -5,32 +5,39 @@ const jwtSecret = require("../config/jwtConfig");
 const User = require("../models/user");
 
 module.exports = app => {
-  app.post('/loginUser', (req, res, next) => {
-    passport.authenticate('login', (err, users, info) => {
-      if (err) {
-        console.error(`Error: ${err}`);
-      }
-      if (info !== undefined) {
-        console.error(info.message);
-        if (info.message === 'Wrong username') {
-          res.status(401).send(info.message);
-        } else {
-          res.status(403).send(info.message);
-        }
-      } else {
-        req.logIn(users, () => {
-          User.findOne({ username: req.body.username }).then(user => {
-            const token = jwt.sign({ id: user._id }, jwtSecret.secret, {
-              expiresIn: 60 * 60,
-            });
-            res.status(200).send({
-              auth: true,
-              token,
-              message: 'User athenticated and logged in',
-            });
-          });
-        });
-      }
-    })(req, res, next);
-  });
+    app.post('/loginUser', (req, res, next) => {
+        axios.post("http://localhost:8083/userLogin", req.body.username)
+            .then(response => {
+                console.log("sent log")
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        passport.authenticate('login', (err, users, info) => {
+            if (err) {
+                console.error(`Error: ${err}`);
+            }
+            if (info !== undefined) {
+                console.error(info.message);
+                if (info.message === 'Wrong username') {
+                    res.status(401).send(info.message);
+                } else {
+                    res.status(403).send(info.message);
+                }
+            } else {
+                req.logIn(users, () => {
+                    User.findOne({ username: req.body.username }).then(user => {
+                        const token = jwt.sign({ id: user._id }, jwtSecret.secret, {
+                            expiresIn: 60 * 60,
+                        });
+                        res.status(200).send({
+                            auth: true,
+                            token,
+                            message: 'User athenticated and logged in',
+                        });
+                    });
+                });
+            }
+        })(req, res, next);
+    });
 };
