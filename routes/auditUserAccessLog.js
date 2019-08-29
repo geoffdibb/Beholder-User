50d876dc05832da490d9073baaeac26d823ec8ec
lushilling <lushilling@hotmail.co.uk>
2019-08-29 09:31:01 +0100

    endpoints

--- routes/auditUserAccessLog.js
+++ routes/auditUserAccessLog.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 const axios = require("axios");
 
 module.exports = (app) => {
-    app.get('/getaudituseraccesslog/:username', (req, res, next) => {
+    app.get('/user/getaudituseraccesslog/:username', (req, res, next) => {
         passport.authenticate('jwt', { session: false }, (err, user, info) => {
             if (info !== undefined) {
                 res.status(401).send(info.message);
